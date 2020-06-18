9fa5469f672feb67318b16e331dc3a789a9c517f
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2020-06-18 15:43:18 +0600

    product details

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,3 +28,54 @@ exports.getIndex = (req, res, next) => {
         })
         .catch(err => new Error(err));
 };
+
+
+exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
+        .then(products => {
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPrevPage: page > 1,
+                nextPage: page + 1,
+                prevPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            res.render('shop/product-details', {
+                product: product,
+                pageTitle: product.title,
+                path: '/products'
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
