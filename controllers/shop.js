37ca9245cf570545a2eeb73ca7ac4f2722258a30
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2020-06-20 10:49:27 +0600

    pdfkit to render order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+const pdfkit = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
-const stripe = require('stripe')(process.env.STRIPE_KEY);
-console.log(process.env.STRIPE_KEY);
+
 const ITEMS_PER_PAGE = 6;
 
 
@@ -204,4 +208,51 @@ exports.getOrders = (req, res, next) => {
             });
         })
         .catch(err => new Error(err));
+};
+
+
+// GET orders/:orderId
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+    Order.findById(orderId)
+        .then(order => {
+            if (!order) {
+                return next(new Error('No order found.'));
+            }
+            if (order.user.userId.toString() !== req.user._id.toString()) {
+                return next(new Error('Unauthorized'));
+            }
+            const invoiceName = 'invoice-' + orderId + '.pdf';
+            const invoicePath = path.join('data', 'invoices', invoiceName);
+            const pdfDoc = new pdfkit();
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader(
+                'Content-Disposition',
+                'inline; filename="' + invoiceName + '"'
+            );
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+            pdfDoc.fontSize(26).text('Invoice', {
+                underline: true
+            });
+            pdfDoc.text('-----------------------');
+            let totalPrice = 0;
+            order.products.forEach(p => {
+                totalPrice += p.quantity * p.product.price;
+                pdfDoc
+                    .fontSize(14)
+                    .text(
+                        p.product.title +
+                        ' - ' +
+                        p.quantity +
+                        ' x ' +
+                        '$' +
+                        p.product.price
+                    );
+            });
+            pdfDoc.text('------------------------------------------');
+            pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+            pdfDoc.end();
+        })
+        .catch(err => next(err));
 };
